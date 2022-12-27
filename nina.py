d14a282a6f61da1fd8a6cd81e6cc0198ccecfcc9
H41stur <leonardor.toledo+1@gmail.com>
2022-12-27 20:45:04 -0300

    Portscan hackertarget function

--- nina.py
+++ nina.py
@@ -15,6 +15,7 @@ import dns.resolver
 import dns.query
 import tldextract
 import wget
+import ast
 from time import sleep
 from prettytable import PrettyTable
 from bs4 import BeautifulSoup as bs 
@@ -49,6 +50,7 @@ def arguments():
     a.add_argument("-a", "--axfr", help="Try a domain zone transfer attack", required=False, action='store_true')
     a.add_argument("--dork", help="Try some dorks", action='store_true', required=False)
     a.add_argument("-s", "--subdomains", help="Do a search for any subdomain registered", required=False, action='store_true')
+    a.add_argument("-p", "--portscan", help="Simple portscan made on https://hackertarget.com/nmap-online-port-scanner", action='store_true', required=False)
     a.add_argument("--subtake", help="Check for subdomain takeover vulnerability", required=False, action='store_true')
     a.add_argument("-t", "--tech", help="Try to discover technologies in the page", required=False, action='store_true')
     a.add_argument("-c", "--cors", help="Try to find CORS misconfigurations", required=False, action='store_true')
@@ -1458,7 +1460,110 @@ def subtake(domain, store, subs, dirFile):
     else:
         print(f"[{Fore.LIGHTYELLOW_EX}!{Fore.RESET}] No subdomain vulnerable.")
 
+# portscan request function
+def portscan_request(s):
+    
+    hackertarget = 'https://hackertarget.com/nmap-online-port-scanner/'
+    header = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36'}
 
+    result = {}
+    partial = {}
+    ports = []
+
+    name_of_nonce_field = ""
+
+    # getting name_of_nonce_field value
+    try:
+        r = requests.get(hackertarget, verify=False)
+    except Exception as e:
+        return result
+
+    soup = bs(r.text, 'html.parser')
+    inputs = soup.find_all('input')
+    for i in inputs:
+        if not name_of_nonce_field:
+            try:
+                if i.attrs["name"] == "name_of_nonce_field":
+                    name_of_nonce_field = i.attrs["value"]
+            except Exception as e:
+                pass
+
+    if name_of_nonce_field:
+        body = {"theinput": s, "thetest": "nmap", "name_of_nonce_field": name_of_nonce_field, "_wp_http_referer": "%2Fnmap-online-port-scanner%2F"}
+
+        r = requests.post(hackertarget, headers=header, data=body)
+        soup = bs(r.text, 'html.parser')
+        scan = soup.find_all('pre')
+        for value in scan:
+            try:
+                if value.attrs["id"] == "formResponse":
+                    line = value.text
+                    line = line.split("\n")
+                    for atr in line:
+                        if atr[:1].isdigit():
+                            atr = " ".join(atr.split())
+                            atr = atr.split(" ")
+                            atr[0] = atr[0].replace("/tcp", "")
+                            partial["port"] = atr[0]
+                            partial["status"] = atr[1]
+                            partial["service"] = atr[2]
+                            if partial not in ports:
+                                ports.append(str(partial))
+            except Exception as e:
+                pass
+
+        if ports:
+            result["host"] = s
+            result["result"] = ports
+
+
+        return result
+
+
+# portscan function
+def portscan(domain, store, dirFile, subs, srcPath):
+
+    print(f"\n{Fore.LIGHTBLUE_EX}[*] Portscanning...\n")
+    sleep(0.2)
+
+    if domain not in subs:
+        subs.append(domain)
+
+    results = []
+
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=THREADS)
+    data = (pool.submit(portscan_request, s) for s in subs)
+    for resp in concurrent.futures.as_completed(data):
+        if resp is not None and resp not in results:
+            resp = resp.result()
+            results.append(resp)
+
+    if results:
+        if store:
+            f = open(dirFile + "/" + domain + ".report.md", "a")
+            f.write(f"\n\n## Portscan Results\n\n")
+            f.close()
+        for i in results:
+            if i:
+                print(f"[{Fore.LIGHTGREEN_EX}+{Fore.RESET}] {i['host']}\n")
+                table = PrettyTable(["Port", "Status", "Service (possible)"])
+                for r in i["result"]:
+                    r = ast.literal_eval(r)
+                    table.add_row([r['port'], r['status'], r['service']])
+                print(table)
+                print("\n")
+
+                if store:
+                    f = open(dirFile + "/" + domain + ".report.md", "a")
+                    f.write(f"\n\n### Host {i['host']}\n")
+                    f.write("|" + " PORT \t\t\t\t| STATUS \t\t\t| SERVICE (Possible)|\n")
+                    f.write("|" + "-"*23 + "|" + "-"*23 + "|" + "-"*23 + "|\n")
+                    for r in i["result"]:
+                        r = ast.literal_eval(r)
+                        f.write(f"|{r['port']}\t\t\t\t|{r['status']}\t\t\t\t|{r['service']}\t\t\t\t|\n")
+                    f.close()
+    else:
+        print(f"[{Fore.LIGHTYELLOW_EX}!{Fore.RESET}] Unable to execute portscan!")
 
 # Program workflow
 if __name__ == "__main__":
@@ -1540,6 +1645,7 @@ if __name__ == "__main__":
             whois_lookup(domain, store, dirFile)
             dns_information(domain, store, dirFile)
             zone_transfer(domain, store, dirFile)
+            portscan(domain, store, dirFile, subs, srcPath)
             if subs:
                 subtake(domain, store, subs, dirFile)
             cors(domain, store, dirFile, subs, srcPath)
@@ -1640,6 +1746,13 @@ if __name__ == "__main__":
     try:
         if parsing.dork:
             dorks(domain, store, dirFile, srcPath)
+    except KeyboardInterrupt:
+        sys.exit(f"[{Fore.LIGHTYELLOW_EX}!{Fore.RESET}] Interrupt handler received, exiting...\n")
+
+    # Portscan
+    try:
+        if parsing.portscan:
+            portscan(domain, store, dirFile, subs, srcPath)
     except KeyboardInterrupt:
         sys.exit(f"[{Fore.LIGHTYELLOW_EX}!{Fore.RESET}] Interrupt handler received, exiting...\n")
 
